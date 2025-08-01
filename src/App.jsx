b6fd658c449a5716ad9f269446b421ee8dfcd12c
Sambit Kumar Padhy <sambeetpadhy2000@gmail.com>
2025-08-01 20:41:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-import CountDown from "./CountDown/CountDown"
-import DigitalClock from "./DigitalClock/DigitalClock"
 import StopWatch from "./StopWatch/StopWatch"
 import React from 'react'
-import ToDo_List from "./ToDo_List/ToDo_List"
-import Counter from "./Counter/Counter"
-import PasswordGenerator from "./PasswordGenerator/PasswordGenerator"
 function App() {
   return (
     <>
-     {/* <StopWatch/> */}
-     {/* <DigitalClock/> */}
-     {/* <CountDown/> */}
-     <ToDo_List/>
-     {/* <Counter/> */}
-     {/* <PasswordGenerator/> */}
+     <StopWatch/>
     </>
   )
 }
